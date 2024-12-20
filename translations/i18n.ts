c0afc598990c9e40f80552113d63733f52046638
wkdu0723 <jinvicky17@gmail.com>
2024-12-20 11:10:40 +0900

    feat: hydration error로 인해서 i18n.ts에 lng 설정 ko로 추가

--- translations/i18n.ts
+++ translations/i18n.ts
@@ -1,36 +1,43 @@
-import i18next from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import enTranslations from '@/translations/json/en.json';
-import koTranslations from '@/translations/json/ko.json';
-import jaTranslations from '@/translations/json/ja.json';
+import enTranslations from "@/translations/json/en.json";
+import koTranslations from "@/translations/json/ko.json";
+import jaTranslations from "@/translations/json/ja.json";
 
 const languageDetectorOptions = {
-    order: ['navigator', 'querystring', 'cookie', 'localStorage', 'subdomain', 'header'],
+  order: [
+    "navigator",
+    "querystring",
+    "cookie",
+    "localStorage",
+    "subdomain",
+    "header",
+  ],
 };
 
 i18next
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        // lng: 'ko',
-        detection: languageDetectorOptions,
-        fallbackLng: 'ko',
-        resources: {
-            en: {
-                translation: enTranslations,
-            },
-            ko: {
-                translation: koTranslations,
-            },
-            ja: {
-                translation: jaTranslations,
-            }
-        },
-        interpolation: {
-            escapeValue: false, // xss 보호를 위해서 escape 설정을 한다.
-        },
-    });
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    lng: "ko",
+    detection: languageDetectorOptions,
+    fallbackLng: "ja",
+    resources: {
+      en: {
+        translation: enTranslations,
+      },
+      ko: {
+        translation: koTranslations,
+      },
+      ja: {
+        translation: jaTranslations,
+      },
+    },
+    interpolation: {
+      escapeValue: false, // xss 보호를 위해서 escape 설정을 한다.
+    },
+  });
 
 export default i18next;
