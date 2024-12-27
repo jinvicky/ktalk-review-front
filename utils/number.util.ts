03e4db1bc22bb83f8c5dbebe1100a6bc154c1a31
jinvicky <jinvicky17@gmail.com>
2024-12-27 10:17:37 +0900

    feat: 수수료 util 추가

--- utils/number.util.ts
+++ utils/number.util.ts
@@ -1,3 +1,9 @@
+/**
+ * 금액을 KRW 기준 ,를 붙인 문자열로 반환
+ * @param price 
+ * @param useFormat 
+ * @returns 
+ */
 export const addCommaKRW = (
   price: number,
   useFormat: boolean = false
@@ -6,3 +12,15 @@ export const addCommaKRW = (
     .toLocaleString("ko-KR", { maximumFractionDigits: 3 })
     .concat(useFormat ? "원" : "");
 };
+
+/**
+ * payApp 수수료를 추가한 금액을 반환
+ * 6.5% 수수료를 올림하여 백 원 단위로 결제
+ * @param price - 원래 금액
+ * @returns 수수료가 추가된 금액 (백 원 단위로 올림 처리됨)
+ */
+export const addPayappFee = (price: number): number => {
+  const fee = price * 0.065;
+  const feeRounded = Math.ceil(fee / 100) * 100;
+  return price + feeRounded;
+}
