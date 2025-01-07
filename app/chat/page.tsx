fcc3583c3182a1826e080c447eb2738d639b200c
jinvicky <jinvicky17@gmail.com>
2025-01-07 18:45:01 +0900

    [test: 테스트]

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,14 +4,9 @@ import ChatRoomList from "@/components/chat/ChatRoomList";
 const ChatPage = () => {
     return (
         <div>
-            <div>Chat Page</div>
-            <ChatRoom />
-
-            <div>Admin Chat Page</div>
             <div className="flex ">
-                {/* 어드민만 ChatRoomList를 보여준다. */}
                 <ChatRoomList />
-                <ChatRoom />
+                {/* <ChatRoom /> */}
             </div>
         </div>
     )
