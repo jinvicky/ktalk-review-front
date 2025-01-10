34018dbe80cf58cc44db0283990de5a2e7773706
jinvicky <jinvicky17@gmail.com>
2025-01-10 16:15:38 +0900

    [feat: test api 추가]

--- app/test/2/page.tsx
+++ app/test/2/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+
 const TestPage2 = () => {
 
-    fetch('/api/chat/test')
+    useEffect(() => {
+        fetch('/api/chat/test')
         .then(res => res.json())
         .then(data => console.log('lets jinvicky', data));
-
+    }, []);
 
 
     return <>this is chat test for jinvicky</>;
