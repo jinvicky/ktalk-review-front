662aa229da5ccc33e7b9af15c0cc7a18dcc1fe17
jinvicky <jinvicky17@gmail.com>
2025-01-20 18:08:23 +0900

    Test: 테스트로 헤더에 credential 추가

--- app/next-api/server/serverUserApi.ts
+++ app/next-api/server/serverUserApi.ts
@@ -2,7 +2,8 @@
 export const fetchFromSpringBoot = async (headerArg: Headers) => {
     const resp = await fetch(process.env.NEXT_PUBLIC_DOMAIN_URL + `/api/user/from-next`, { // boot의 api
         method: 'POST', 
-        headers: headerArg
+        headers: headerArg, 
+        credentials: 'include',
     });
 
     return await resp.json();
