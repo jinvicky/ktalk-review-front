e7be8a590acef29f126ac0378d8bcf4d5d3b7f07
jinvicky <jinvicky17@gmail.com>
2025-01-25 22:24:52 +0900

    Fix: 미들웨어 회원가입 경로 오타 수정

--- app/next-api/sign-in/route.ts
+++ app/next-api/sign-in/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { signIn } from "@/api/userApi";
 
-/**
- *  여기서 spring boot로 로그인 요청을 보낸 다음에, 
- *  응답이 200으로 오면 next.js가 쿠키를 생성해서 응답에 추가한다. (next.js 환경에 쿠키 저장)
- */
 export async function POST(request: NextRequest) {
     const form = await request.json();
     const respFromBoot = await signIn(form) as ApiResult<number>;
+
+    const duration = new Date(Date.now() + 1000 * 60 * 60 * 3).toUTCString(); // 3시간
 
     return new NextResponse(
         JSON.stringify({ message: 'OK' }),
@@ -15,7 +13,7 @@ export async function POST(request: NextRequest) {
             status: 200,
             headers: {
                 'Content-Type': 'application/json',
-                'Set-Cookie': `userSession=${respFromBoot.data}; HttpOnly; Secure; Path=/; Expires=${new Date(Date.now() + 1000 * 60 * 60 * 24 * 7).toUTCString()};`,
+                'Set-Cookie': `userSession=${respFromBoot.data}; HttpOnly; Secure; Path=/; Expires=${duration};`,
             },
         }
     );
--- middleware.ts
+++ middleware.ts
@@ -5,24 +5,24 @@ import { cookies } from 'next/headers'
 
 export async function middleware(request: NextRequest) {
     const cookieStore = cookies();
-    const userSession = cookieStore.get('userSession'); // 나중에 쿠키명 userSession으로 변경
+    const userSession = cookieStore.get('userSession');
     /**
      * userSession은 {name: '', value:''} 형태다.
      */
     const currentPath = request.nextUrl.pathname;
-
-    console.log('userSession', userSession?.value);
-    if (!userSession?.value) {
-        if (currentPath === '/user/sign-in' || currentPath === 'user/sign-up') {
+    
+    if (userSession === undefined || !userSession?.value) {
+        
+        if (currentPath === '/user/sign-in' || currentPath === '/user/sign-up') {
             // 로그인을 안 했는데 로그인/회원가입으로 오면 그냥 통과
             return NextResponse.next();
         }
         // 그외 전부 로그인 페이지로 이동
         return NextResponse.redirect(new URL('/user/sign-in', request.url));
     } else {
-        // 로그인을 했는데 로그인/회원가입으로 오면 메인으로 보내기
+        // 로그인을 했는데 로그인/회원가입으로 오면 메인(promotion)으로 보내기
         if (currentPath === '/user/sign-in' || currentPath === '/user/sign-up') {
-            return NextResponse.redirect(new URL('/', request.url));
+            return NextResponse.redirect(new URL('/promotion', request.url));
         }
     }
     return NextResponse.next();
