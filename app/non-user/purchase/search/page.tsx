54af4992bc0581415caea7f66e87117e9363b1f0
jinvicky <jinvicky17@gmail.com>
2025-02-02 23:00:39 +0900

    Feat: 비회원 주문 조회 폼 추가

---
+++ app/non-user/purchase/search/page.tsx
@@ -0,0 +1,15 @@
+
+
+
+import React from 'react';
+import NonUserPurchaseSearchForm from './components/SearchForm';
+
+const NonUserPurchaseSearchPage = () => {
+    return (
+        <div>
+            <NonUserPurchaseSearchForm />
+        </div>
+    );
+}
+
+export default NonUserPurchaseSearchPage;
