98caf9d2d081fd9ed997ab6551a014f2e26ccacf
jinvicky <jinvicky17@gmail.com>
2025-02-14 23:50:16 +0900

    Feat: 결제 요청, 결제 내역 정보 조회

--- app/non-user/commission/apply/[applyId]/page.tsx
+++ app/non-user/commission/apply/[applyId]/page.tsx
@@ -1,59 +1,88 @@
-import { selectCommissionApplyById } from "@/api/commissionApplyApi";
 import NonUserReviewModalButton from "./NonUserReviewModalButton";
 
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+// import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-
 
 /**
  * 신청서, 결제 내역 상세
  */
 const NonUserCommissionApplyDetailPage: React.FC<{ params: { applyId: string } }> = async ({ params }) => {
     const applyId = params.applyId;
-    const data = await selectCommissionApplyById(applyId) as ApiResult<CommissionApply>; // 신청서, 결제요청내역, 결제 내역, 리뷰 내역까지 통으로 조회하는 API 필요
+    const data = await fetch(process.env.NEXT_PUBLIC_DOMAIN_URL + `/api/commission/apply/contract/${applyId}`).then(async (res) => await res.json());
 
-    const { title, content } = data.data;
+    const { applyVO, payRequestVO, paymentVO, reviewVO } = data.data;
 
     return <div className="p-5">
-        <div className="flex gap-3 items-center">
-            <h1 className="text-2xl font-bold">{title}</h1>
+        <div className="">
+            {/* <h1 className="text-2xl font-bold">{title}</h1> */}
+            <div className="py-3 text-gray-500 font-bold">
+                * 비회원 신청서는 보안을 위해 작업이 완료된 후 삭제됩니다.
+            </div>
         </div>
-        <div className="flex items-center py-10">
-            <h1 className="text-2xl font-bold">리뷰 내역</h1>
+        <div>
+            <div className="flex items-center py-5">
+                <h1 className="text-2xl font-bold">리뷰 내역</h1>
+                <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
+            </div>
             <NonUserReviewModalButton
                 applyId={applyId}
             />
-            <button>
-                <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
-            </button>
+            {
+                reviewVO ? <div>
+                    {reviewVO.content}
+                </div>
+                    : <div className="py-5">아직 작성된 리뷰가 없습니다.</div>
+            }
         </div>
         <div className="flex items-center py-10">
             <h1 className="text-2xl font-bold">신청서 내역</h1>
-            <button>
-                <KeyboardArrowUpIcon style={{ fontSize: 50 }} />
-            </button>
         </div>
-        <div className="flex items-center py-10">
-            <h1 className="text-2xl font-bold">결제요청서 내역</h1>
-            <button>
-                <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
-            </button>
-        </div>
-        <div className="flex items-center py-10">
-            <h1 className="text-2xl font-bold">결제 내역</h1>
-            <button>
-                <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
-            </button>
+        <div>
+            <div className="flex items-center py-5">
+                <h1 className="text-2xl font-bold">결제요청서 내역</h1>
+                <button>
+                    <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
+                </button>
+            </div>
+            <div className="[&>div]:font-bold [&>div]:text-gray-500 [&>div]:py-3">
+                {payRequestVO ? <>
+                    <div>결제 요청 금액: {payRequestVO.price}</div>
+
+                    <div>
+                        협의사항: {payRequestVO.discussion}
+                    </div>
+                    <div>
+                        결제여부: {payRequestVO.payYn === "Y" ? "결제완료" : "미결제"}
+                    </div>
+                    <div>
+                        결제요청날짜: {payRequestVO.rgtrDtFomatted}
+                    </div>
+                </> : <div className="py-5">결제 요청서가 없습니다.</div>
+                }
+            </div>
         </div>
 
-
-        {/* <div className="text-xl font-bold mt-5">
-            {title}
-        </div> */}
-        <div className="mt-5">
-            {content}
+        <div>
+            <div className="flex items-center py-5">
+                <h1 className="text-2xl font-bold">결제 내역</h1>
+                <button>
+                    <KeyboardArrowDownIcon style={{ fontSize: 50 }} />
+                </button>
+            </div>
+            <div className="[&>div]:font-bold [&>div]:text-gray-500 [&>div]:py-3">
+                {paymentVO ? <>
+                    <div>결제 요청 금액: {paymentVO.price}</div>
+                    <div>
+                        결제자명: {paymentVO.userName}
+                    </div>
+                    <div>
+                        결제날짜: {payRequestVO.payDtFomatted}
+                    </div>
+                </> : <div className="py-5">결제 정보가 없습니다.</div>
+                }
+            </div>
         </div>
-    </div>
+    </div >
 }
 
 export default NonUserCommissionApplyDetailPage;
