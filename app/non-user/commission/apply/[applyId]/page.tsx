dd36b7774219ef767115a76c19a6a1d19eed878c
jinvicky <jinvicky17@gmail.com>
2025-02-18 07:05:40 +0900

    Feat: 멀티업로드 테스트 화면 추가

--- app/non-user/commission/apply/[applyId]/page.tsx
+++ app/non-user/commission/apply/[applyId]/page.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 
 // import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+// import CopyToClipboard from "react-copy-to-clipboard";
+// import Link from "next/link";
+import PaymentRequestForm from "@/components/payment/PaymentRequestForm";
 
 /**
  * 신청서, 결제 내역 상세
@@ -21,7 +24,7 @@ const NonUserCommissionApplyDetailPage: React.FC<{ params: { applyId: string } }
         <div className="">
             <h1 className="text-2xl font-bold">{applyVO.title}</h1>
             <div className="py-3 text-gray-500 font-bold">
-                * 비회원 신청서는 보안을 위해 작업이 완료된 후 3일 내로 삭제됩니다.
+                * 비회원 신청서는 보안을 위해 작업이 완료된 후 2일 내로 삭제됩니다.
             </div>
         </div>
         <div>
@@ -56,7 +59,7 @@ const NonUserCommissionApplyDetailPage: React.FC<{ params: { applyId: string } }
                 </button>
             </div>
             <div className="[&>div]:font-bold [&>div]:text-gray-500 [&>div]:py-3">
-                {payRequestVO ? <>
+                {applyVO ? <>
                     <div>
                         신청자명: {applyVO.userName}
                     </div>
@@ -136,7 +139,7 @@ const NonUserCommissionApplyDetailPage: React.FC<{ params: { applyId: string } }
                     <div>
                         결제날짜: {paymentVO.payDtFomatted}
                     </div>
-                </> : <div className="py-5">결제 정보가 없습니다.</div>
+                </> : payRequestVO ? <PaymentRequestForm data={payRequestVO} /> : <div className="py-5">결제 요청서가 있어야 결제할 수 있습니다.</div>
                 }
             </div>
         </div>
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,34 @@
 "use client";
 
+import { useState } from "react";
+
 const Test = () => {
+
+  const[fileList, setFileList] = useState<FileList | null>(null); 
+
+  const onClick = async () => {
+    const formData = new FormData();
+    if(!fileList) return;
+
+    for (let i = 0; i < fileList.length; i++) {
+      formData.append("files", fileList[i]);
+    }
+    const resp = await fetch("/api/commission/apply/test", {
+      method: "POST",
+      body: formData,
+    })
+
+    console.log(resp);
+  }
 
   return (
     <>
-      
+
+      <input type="file" multiple onChange={(e)=> setFileList(e.target.files)}/>
+
+      <button onClick={onClick}>
+        클릭시 업로드 요청
+      </button>
 
     </>
   );
