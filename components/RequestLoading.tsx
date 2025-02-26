b480edf7aa8cc69f649346924548df0545a2778d
jinvicky <jinvicky17@gmail.com>
2025-02-26 23:34:19 +0900

    Feat: 로딩 오퍼시티 수정

--- components/RequestLoading.tsx
+++ components/RequestLoading.tsx
@@ -2,7 +2,7 @@ import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt
 
 export const RequestLoading = ({ message }: { message?: string }) => {
     return (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 z-50 flex items-center justify-center">
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 z-50 flex items-center justify-center">
             <div className="h-50 text-white font-bold text-2xl text-center">
                 <SentimentSatisfiedAltIcon sx={{ fontSize: 120 }} />
                 <div className="my-2">
